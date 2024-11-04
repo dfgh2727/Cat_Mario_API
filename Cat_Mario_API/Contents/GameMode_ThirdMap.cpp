37a89dfc824432d759049bcf0bbd1bb9e65ca778
dfgh2727 <dfgh2727@naver.com>
2024-11-04 11:53:41 +0900

    이수환: GameMode_ThirdMap 추가

---
+++ Cat_Mario_API/Contents/GameMode_ThirdMap.cpp
@@ -0,0 +1,37 @@
+#include "PreCompile.h"
+
+#include "GameMode_ThirdMap.h"
+
+#include <EngineCore/Level.h>
+#include <EnginePlatform/EngineInput.h>
+#include <EngineCore/EngineAPICore.h>
+
+#include "ThirdMap.h"
+
+GameMode_ThirdMap::GameMode_ThirdMap()
+{
+}
+
+GameMode_ThirdMap::~GameMode_ThirdMap()
+{
+}
+
+void GameMode_ThirdMap::BeginPlay()
+{
+	{
+		ThirdMap* NewActor = GetWorld()->SpawnActor<ThirdMap>();
+
+	}
+}
+
+void GameMode_ThirdMap::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	if (true == UEngineInput::GetInst().IsDown(VK_RETURN))
+	{
+		UEngineAPICore::GetCore()->OpenLevel("Play_ThirdMap");
+	}
+
+}
+
---
+++ Cat_Mario_API/Contents/GameMode_ThirdMap.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <EngineCore/GameMode.h>
+
+
+// Ό³Έν :
+class GameMode_ThirdMap : public AGameMode
+{
+public:
+	// constrcuter destructer
+	GameMode_ThirdMap();
+	~GameMode_ThirdMap();
+
+	// delete Function
+	GameMode_ThirdMap(const GameMode_ThirdMap& _Other) = delete;
+	GameMode_ThirdMap(GameMode_ThirdMap&& _Other) noexcept = delete;
+	GameMode_ThirdMap& operator=(const GameMode_ThirdMap& _Other) = delete;
+	GameMode_ThirdMap& operator=(GameMode_ThirdMap&& _Other) noexcept = delete;
+
+	void BeginPlay() override;
+
+	void Tick(float _DeltaTime) override;
+
+protected:
+
+private:
+
+};
+
