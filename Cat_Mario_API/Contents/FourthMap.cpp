4450f6a52dc2004f28992e896dbfaca780909a3d
dfgh2727 <dfgh2727@naver.com>
2024-11-04 12:15:09 +0900

    이수환: FourthMap 추가

---
+++ Cat_Mario_API/Contents/FourthMap.cpp
@@ -0,0 +1,24 @@
+#include "PreCompile.h"
+
+#include "FourthMap.h"
+
+#include <EngineCore/EngineAPICore.h>
+#include <EngineCore/SpriteRenderer.h>
+#include "Enum.h"
+
+
+FourthMap::FourthMap()
+{
+	USpriteRenderer* SpriteRenderer = CreateDefaultSubObject<USpriteRenderer>();
+	SpriteRenderer->SetOrder(ERenderOrder::MAP);
+	SpriteRenderer->SetSprite("4thMap.png");
+
+	FVector2D MapScale = SpriteRenderer->SetSpriteScale(1.0f);
+	SpriteRenderer->SetComponentLocation(MapScale.Half());
+}
+
+
+FourthMap::~FourthMap()
+{
+}
+
---
+++ Cat_Mario_API/Contents/FourthMap.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Actor.h"
+
+
+// Ό³Έν :
+class FourthMap : public AActor
+{
+public:
+	// constrcuter destructer
+	FourthMap();
+	~FourthMap();
+
+	// delete Function
+	FourthMap(const FourthMap& _Other) = delete;
+	FourthMap(FourthMap&& _Other) noexcept = delete;
+	FourthMap& operator=(const FourthMap& _Other) = delete;
+	FourthMap& operator=(FourthMap&& _Other) noexcept = delete;
+
+protected:
+
+private:
+
+};
+
