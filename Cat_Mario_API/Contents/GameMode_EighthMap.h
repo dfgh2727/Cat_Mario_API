4cebe33980052091a5e6a3fe719645a315325ed5
dfgh2727 <dfgh2727@naver.com>
2024-11-04 12:53:30 +0900

    이수환: Release 빌드 및 렌더 성공

--- Cat_Mario_API/Contents/GameMode_EighthMap.h
+++ Cat_Mario_API/Contents/GameMode_EighthMap.h
@@ -14,7 +14,7 @@ public:
 	GameMode_EighthMap(GameMode_EighthMap&& _Other) noexcept = delete;
 	GameMode_EighthMap& operator=(const GameMode_EighthMap& _Other) = delete;
 	GameMode_EighthMap& operator=(GameMode_EighthMap&& _Other) noexcept = delete;
-
+	
 	void BeginPlay();
 
 	//void Tick(float _DeltaTime) override;
