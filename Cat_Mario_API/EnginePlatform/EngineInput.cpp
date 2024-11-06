aaafec7b36a2bdc3d060f6ae8d23ecc8d9f990a7
dfgh2727 <dfgh2727@naver.com>
2024-11-06 16:25:16 +0900

    이수환: EnginePlatform 업데이트

--- Cat_Mario_API/EnginePlatform/EngineInput.cpp
+++ Cat_Mario_API/EnginePlatform/EngineInput.cpp
@@ -195,8 +195,6 @@ UEngineInput::UEngineInput()
 	Keys.insert({ VK_F22		, UEngineKey(VK_F22) });
 	Keys.insert({ VK_F23		, UEngineKey(VK_F23) });
 	Keys.insert({ VK_F24		, UEngineKey(VK_F24) });
-	Keys.insert({ VK_RETURN		, UEngineKey(VK_RETURN) });
-
 
 }
 
