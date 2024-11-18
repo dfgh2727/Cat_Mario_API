ab3077ce68cee88f76850294d510ab53cfd52f53
dfgh2727 <dfgh2727@naver.com>
2024-11-18 15:35:26 +0900

    이수환: 몬스터 구현 시작

---
+++ Cat_Mario_API/Contents/WhiteCircle.cpp
@@ -0,0 +1,27 @@
+#include "PreCompile.h"
+#include "WhiteCircle.h"
+#include "Enum.h"
+
+#include <EngineCore/SpriteRenderer.h>
+#include <EnginePlatform/EngineInput.h>
+#include <EngineCore/EngineCoreDebug.h>
+#include <EngineCore/Level.h>
+
+WhiteCircle::WhiteCircle()
+{
+}
+
+WhiteCircle::~WhiteCircle()
+{
+}
+
+void WhiteCircle::BeginPlay()
+{
+
+}
+
+void WhiteCircle::Tick(float _DeltaTime)
+{
+
+}
+
