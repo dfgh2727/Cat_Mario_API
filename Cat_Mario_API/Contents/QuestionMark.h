bab91b8ebf6825a7cd7508cd85bd87aab7cd2041
dfgh2727 <dfgh2727@naver.com>
2024-11-21 15:53:09 +0900

    이수환: QMark 추가

---
+++ Cat_Mario_API/Contents/QuestionMark.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <EngineCore/Actor.h>
+#include <EngineCore/2DCollision.h>
+
+// Ό³Έν :
+class QuestionMark : public AActor
+{
+public:
+	// constrcuter destructer
+	QuestionMark();
+	~QuestionMark();
+
+	// delete Function
+	QuestionMark(const QuestionMark& _Other) = delete;
+	QuestionMark(QuestionMark&& _Other) noexcept = delete;
+	QuestionMark& operator=(const QuestionMark& _Other) = delete;
+	QuestionMark& operator=(QuestionMark&& _Other) noexcept = delete;
+
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+protected:
+
+private:
+	class USpriteRenderer* QMarkRenderer;
+	U2DCollision* CollisionComponent;
+};
+
