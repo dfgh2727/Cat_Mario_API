2b907d301c37348360d51552d21ee68e0732aa1c
dfgh2727 <dfgh2727@naver.com>
2024-11-28 15:55:50 +0900

    이수환: FB

--- Cat_Mario_API/Contents/FBPipe.cpp
+++ Cat_Mario_API/Contents/FBPipe.cpp
@@ -3,6 +3,7 @@
 
 #include "Enum.h"
 
+#include <EngineCore/Level.h>
 #include <EngineCore/SpriteRenderer.h>
 #include <EnginePlatform/EngineInput.h>
 #include <EngineCore/EngineCoreDebug.h>
@@ -30,10 +31,26 @@ void FBPipe::BeginPlay()
 void FBPipe::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
+
+	if (SwitchUp == false)
+	{
+
+	}
+	else
+	{
+
+	}
+
 }
 
 void FBPipe::ShootFB(float _DeltaTime)
 {
+	FVector2D PipePos = this->GetActorLocation();
+	FireBall* FB = GetWorld()->SpawnActor<FireBall>();
+	FB->SetActorLocation(PipePos);
+
+	FVector2D FBVelocity = FVector2D::ZERO;
+	FBVelocity.X = (-1)^
 
 }
 
--- Cat_Mario_API/Contents/FireBall.cpp
+++ Cat_Mario_API/Contents/FireBall.cpp
@@ -30,3 +30,4 @@ void FireBall::Gravity(float _DeltaTime)
 	AddActorLocation(GravityForce * _DeltaTime);
 	GravityForce += FVector2D::DOWN * _DeltaTime * 1200.0f;
 }
+
