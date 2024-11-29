0a3e04bb78308df8b818d2d25aac47a2794397f9
dfgh2727 <dfgh2727@naver.com>
2024-11-29 13:10:36 +0900

    이수환:

--- Cat_Mario_API/Contents/InfiniteCoinBox.cpp
+++ Cat_Mario_API/Contents/InfiniteCoinBox.cpp
@@ -1,6 +1,13 @@
 #include "PreCompile.h"
 #include "InfiniteCoinBox.h"
+#include "Enum.h"
+#include "BNormalBlock.h"
 
+#include <EngineCore/SpriteRenderer.h>
+#include <EnginePlatform/EngineInput.h>
+#include <EngineCore/EngineCoreDebug.h>
+#include <EngineCore/Level.h>
+#include"MarioCat.h"
 
 InfiniteCoinBox::InfiniteCoinBox()
 {
@@ -18,4 +25,22 @@ void InfiniteCoinBox::BeginPlay()
 void InfiniteCoinBox::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
+}
+
+void InfiniteCoinBox::BlockDisappear(float _DeltaTime)
+{
+	MarioCat* MainActor = GetWorld()->GetPawn<MarioCat>();
+	FVector2D GForce = MainActor->GetGravityForce();
+	FVector2D CatJumpPower = MainActor->GetJumpPower();
+
+	if (CatJumpPower.Y * (-1.0f) >= GForce.Y)
+	{
+		AActor* Result = CollisionComponent->CollisionOnce(ECollisionGroup::PlayerHead);
+		if (nullptr != Result)
+		{
+			CoinShowUP();
+			this->Destroy();
+		}
+
+	}
 }
